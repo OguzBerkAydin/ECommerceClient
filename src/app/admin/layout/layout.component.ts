498c2d4da75acb034b37a18cd5d660ec6674ca61
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-04-14 15:06:32 +0300

    alertify added and customized

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType } from '../../services/admin/alertify.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
 
+  constructor (private alertify: AlertifyService) {}
+  ngOnInit(): void{
+    this.alertify.message("test",MessageType.Notify);
+  }
+  
+  
 }
+
